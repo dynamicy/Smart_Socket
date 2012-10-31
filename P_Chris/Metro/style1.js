748b0ac33ac8f77e8477cf409f50a783759964d5
dynamicy <fermat.little.theorem@gmail.com>
2012-10-31 15:38:18 +0800

    Merge weather with metro.

---
+++ P_Chris/Metro/style1.js
@@ -0,0 +1,15 @@
+siteTitle = "Metro UI Template";
+	siteTitleHome = "Home";
+	mobileDevice = "";
+	theme = "theme_default";   
+    <script type="text/javascript" language="javascript" src="jquery.min.js"></script> 
+    <script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="jquery182.js"><\/script>')</script>
+    <script type="text/javascript" src="compressed.js" /></script> 
+    <script type='text/javascript'>var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-30159978-1']);_gaq.push(['_trackPageview']);
+    (function(){var ga = document.createElement('script'); 
+    ga.type = 'text/javascript'; 
+    ga.async = true;
+    ga.src = 'ga.js';
+    var s = document.getElementsByTagName('script')[0]; 
+    s.parentNode.insertBefore(ga, s);
+    })();
